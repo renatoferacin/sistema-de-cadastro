456384b51554bde28bbe5864e53d8277327abcf2
renatoferacin <renatoferacin@gmail.com>
2022-06-30 21:44:40 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 conseole.log("aplicação está se conectando ao banco de dados")
+//mais um console log de teste:
+console.log("alteração feita nesse commit sem vocer na aula!")
+console.log("Enviando par o GitHub")
